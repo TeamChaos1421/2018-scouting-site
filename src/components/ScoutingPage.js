4e880f944e047663e62bc1b7a0b3514701c58c31
Luc Lagarde <matieu.lagarde@usm.edu>
2018-03-06 14:01:32 -0600

    Make scouting form controlled component

--- src/components/ScoutingPage.js
+++ src/components/ScoutingPage.js
@@ -45,10 +45,33 @@ function UserPrompt(props) {
 }
 
 class ScoutingPage extends React.Component {
+	constructor(props) {
+		super(props);
+		this.state = {};
+
+		this.handleChange = this.handleChange.bind(this);
+		this.handleSubmit = this.handleSubmit.bind(this);
+	}
+
 	componentWillMount() {
 		// TODO; Get regional from DB
 		this.props.allActions.fetchSettings();
 		this.props.tbaActions.fetchMatchData('2018mxmo');
+	}
+
+	handleChange(event) {
+		let target = event.target;
+		let name = target.name;
+		let value = (target.type === 'checkbox') ? target.checked : target.value;
+
+		this.setState({
+			[name]: value
+		});
+	}
+
+	handleSubmit(event) {
+		console.log(this.state);
+		event.preventDefault();
 	}
 
 	render() {
@@ -68,31 +91,31 @@ class ScoutingPage extends React.Component {
 
 					<UserPrompt users={this.props.settings.users} alliances={match.alliances} />
 
-					<form>
+					<form onSubmit={this.handleSubmit}>
 						<h3>Match Info</h3>
-						Username: <input type='text' name='username' /><br/>
-						Match Number: <input type='text' name='matchNumber' /><br/>
-						Team Number: <input type='text' name='teamNumber' /><br/>
+						Username: <input name='username' type='text' value={this.state.username} onChange={this.handleChange} /><br/>
+						Match Number: <input name='matchNumber' type='text' value={this.state.matchNumber} onChange={this.handleChange} /><br/>
+						Team Number: <input name='teamNumber' type='text' value={this.state.value} onChange={this.handleChange} /><br/>
 
 						<h3>Autonomous</h3>
-						<input type='checkbox' name='crossesAuto' /> Crosses auto line<br/>
-						<input type='checkbox' name='scoresScale' /> Attempts to place cube on scale<br/>
-						<input type='checkbox' name='scoresSwitch' /> Attempts to place cube on switch<br/>
+						<input name='crossesAuto' type='checkbox' checked={this.state.crossesAuto} onChange={this.handleChange} /> Crosses auto line<br/>
+						<input name='autoScaleAttempt' type='checkbox' checked={this.state.autoScaleAttempt} onChange={this.handleChange} /> Attempts to place cube on scale<br/>
+						<input name='autoSwitchAttempt' type='checkbox' checked={this.state.autoSwitchAttempt} onChange={this.handleChange} /> Attempts to place cube on switch<br/>
 						<br/>
-						How many cubes scored on scale during autonomous? <input type='number' name='autoScaleScore' /><br/>
-						How many cubes scored on switch during autonomous? <input type='number' name='autoSwitchScore' /><br/>
+						How many cubes scored on scale during autonomous? <input name='autoScaleScore' value={this.autoScaleScore} onChange={this.handleChange} /><br/>
+						How many cubes scored on switch during autonomous? <input name='autoSwitchScore' value={this.autoSwitchScore} onChange={this.handleChange} /><br/>
 
 						<h3>Teleop</h3>
-						How many cubes scored on scale during teleop? <input type='number' name='autoScaleScore' /><br/>
-						How many cubes scored on switch during teleop? <input type='number' name='autoSwitchScore' /><br/>
+						How many cubes scored on scale during teleop? <input name='teleopScaleScore' type='number' value={this.teleopScaleScore} onChange={this.handleChange} /><br/>
+						How many cubes scored on switch during teleop? <input name='teleopSwitchScore' type='number' value={this.teleopSwitchScore} onChange={this.handleChange} /><br/>
 
 						<h3>Endgame</h3>
 						Does the robot climb?
-						<input type='radio' name='climbs' value='1' /> Yes
-						<input type='radio' name='climbs' value='0' /> No
+						<input name='climbs' type='radio' checked={this.state.climbs === '1'} value='1' onChange={this.handleChange} /> Yes
+						<input name='climbs' type='radio' checked={this.state.climbs === '0'} value='0' onChange={this.handleChange} /> No
 
 						<h3>Notes</h3>
-						<textarea name='notes' cols='40' rows='5'/>
+						<textarea name='notes' cols='40' rows='5' value={this.state.notes} onChange={this.handleChange} />
 
 						<br/><br/>
 						<input type='submit' />
