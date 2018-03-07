fe4bc759d36307c3d267d4faecfddde94fe642f5
Luc Lagarde <mlaga97@gmail.com>
2018-03-06 22:41:15 -0600

    Make more better looking

--- src/components/ScoutingForm.js
+++ src/components/ScoutingForm.js
@@ -1,50 +1,196 @@
 // Library imports
 import React from 'react';
 import {Field, reduxForm} from 'redux-form';
-import {FormGroup, FormControl, ControlLabel} from 'react-bootstrap';
+import {FormGroup, FormControl, ControlLabel, Checkbox, Radio, Button, ToggleButton, ToggleButtonGroup} from 'react-bootstrap';
 
 function ReduxFormControl({input, meta, ...props}) {
 	return <FormControl {...props} {...input} />;
 }
 
+function ReduxCheckbox({input, meta, ...props}) {
+	return <Checkbox {...props} {...input} />;
+}
+
+function ReduxRadio({input, meta, ...props}) {
+	return <Radio {...props} {...input} />;
+}
+
+function ReduxToggleButtonGroup({input, meta, ...props}) {
+	return <ToggleButtonGroup {...props} {...input} />
+}
+
 let ScoutingForm = (props) => {
 	return (
 		<form onSubmit={props.handleSubmit}>
-			<h3>Match Info</h3>
-
 			<FormGroup>
 				<ControlLabel>Username</ControlLabel>
 				<Field component={ReduxFormControl} name='username' type='text'/>
 			</FormGroup>
-				
-			Match Number: <Field name='matchNumber' component='input' type='text' /><br/>
-			Team Number: <Field name='teamNumber' component='input' type='text' /><br/>
+
+			<FormGroup>
+				<ControlLabel>Match Number</ControlLabel>
+				<Field component={ReduxFormControl} name='matchNumber' type='text'/>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel>Team Number</ControlLabel>
+				<Field component={ReduxFormControl} name='teamNumber' type='text'/>
+			</FormGroup>
+
+			<hr/>
 
 			<h3>Autonomous</h3>
-			<Field name='crossesAuto' component='input' type='checkbox' /> Crosses auto line<br/>
-			<Field name='autoScaleAttempt' component='input' type='checkbox' /> Attempts to place cube on scale<br/>
-			<Field name='autoSwitchAttempt' component='input' type='checkbox' /> Attempts to place cube on switch<br/>
-			<br/>
-			How many cubes scored on scale during autonomous? <Field name='autoScaleScore' component='input' type='text' /><br/>
-			How many cubes scored on switch during autonomous? <Field name='autoSwitchScore' component='input' type='text' /><br/>
+
+			<FormGroup>
+				<ControlLabel>Moves During Autonomous</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='autoMoves' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel>Crosses auto-line</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='autoCrosses' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel><em>Attempts</em> to place a cube on the scale</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='autoScaleAttempt' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel><em>Attempts</em> to place a cube on the switch</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='autoSwitchAttempt' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+				
+			<FormGroup>
+				<ControlLabel>Number of cubes <em>scored</em> on scale during autonomous</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='autoScaleScore' type='radio'>
+					<ToggleButton value='0'>0</ToggleButton>
+					<ToggleButton value='1'>1</ToggleButton>
+					<ToggleButton value='2'>2</ToggleButton>
+					<ToggleButton value='3'>3</ToggleButton>
+					<ToggleButton value='4'>4</ToggleButton>
+					<ToggleButton value='5'>5+</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel>Number of cubes <em>scored</em> on switch during autonomous</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='autoSwitchScore' type='radio'>
+					<ToggleButton value='0'>0</ToggleButton>
+					<ToggleButton value='1'>1</ToggleButton>
+					<ToggleButton value='2'>2</ToggleButton>
+					<ToggleButton value='3'>3</ToggleButton>
+					<ToggleButton value='4'>4</ToggleButton>
+					<ToggleButton value='5'>5+</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<hr/>
 
 			<h3>Teleop</h3>
-			<Field name='teleopScaleAttempt' component='input' type='checkbox' /> Attempts to place cube on scale<br/>
-			<Field name='teleopSwitchAttempt' component='input' type='checkbox' /> Attempts to place cube on switch<br/>
-			<br/>
-			How many cubes scored on scale during teleop? <Field name='teleopScaleScore' component='input' type='number' /><br/>
-			How many cubes scored on switch during teleop? <Field name='teleopSwitchScore' component='input' type='number' /><br/>
+
+			<FormGroup>
+				<ControlLabel>Moves During Teleop</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='teleopMoves' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel><em>Attempts</em> to place a cube on the scale</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='teleopScaleAttempt' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel><em>Attempts</em> to place a cube on the switch</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='teleopSwitchAttempt' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+				
+			<FormGroup>
+				<ControlLabel>Number of cubes <em>scored</em> on scale during teleop</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='teleopScaleScore' type='radio'>
+					<ToggleButton value='0'>0</ToggleButton>
+					<ToggleButton value='1'>1</ToggleButton>
+					<ToggleButton value='2'>2</ToggleButton>
+					<ToggleButton value='3'>3</ToggleButton>
+					<ToggleButton value='4'>4</ToggleButton>
+					<ToggleButton value='5'>5+</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel>Number of cubes <em>scored</em> on switch during teleop</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='teleopSwitchScore' type='radio'>
+					<ToggleButton value='0'>0</ToggleButton>
+					<ToggleButton value='1'>1</ToggleButton>
+					<ToggleButton value='2'>2</ToggleButton>
+					<ToggleButton value='3'>3</ToggleButton>
+					<ToggleButton value='4'>4</ToggleButton>
+					<ToggleButton value='5'>5+</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<hr/>
 
 			<h3>Endgame</h3>
-			Does the robot climb?
-			<Field name='climbs' component='input' type='radio' value='1' /> Yes
-			<Field name='climbs' component='input' type='radio' value='0' /> No
+
+			<FormGroup>
+				<ControlLabel><em>Attempts</em> to climb</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='climbs' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<FormGroup>
+				<ControlLabel><em>Successfully</em> climbs</ControlLabel>
+				<br/>
+				<Field component={ReduxToggleButtonGroup} name='climbs' type='radio'>
+					<ToggleButton value='1'>Yes</ToggleButton>
+					<ToggleButton value='0'>No</ToggleButton>
+				</Field>
+			</FormGroup>
+
+			<hr/>
 
 			<h3>Notes</h3>
-			<Field name='notes' component='textarea' cols='40' rows='5' />
 
-			<br/><br/>
-			<input type='submit' />
+			<FormGroup>
+				<Field component={ReduxFormControl} name='notes' rows='4' componentClass='textarea'/>
+			</FormGroup>
+
+			<Button type='submit'>Submit</Button>
 		</form>
 	)
 };
