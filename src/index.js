1c9c34a2ed06511bd5773f98e347ff4ef1ab3abf
Luc Lagarde <mlaga97@gmail.com>
2018-03-06 18:28:21 -0600

    Add react-bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,9 @@ import ReactDOM from 'react-dom';
 import {Provider} from 'react-redux';
 import {BrowserRouter} from 'react-router-dom';
 
+// Bootstrap Stuff
+import 'bootstrap/dist/css/bootstrap.min.css';
+
 // Configuration stuff
 import configureStore from './configureStore';
 
